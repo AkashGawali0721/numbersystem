1c67463a0095239766013d50f6e6628cd36dd8a0
Akash Gawali <gawaliakash43@gmail.com>
2024-11-18 17:07:02 +0530

    All activitys are done

---
+++ app.js
@@ -0,0 +1,19 @@
+const numbers = [
+    19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
+  ];
+
+  let c=0;
+  
+  for(let i=0;i<numbers.length;i++){
+    if(i%2 ==0){
+      c++
+  }
+}
+  if(c==2)
+    {
+    console.log(numbers);
+
+  }
+  else{
+   console.log("not prime");
+  }
